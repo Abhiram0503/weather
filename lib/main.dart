f6095ab3903221519bb2e8b17e3c44307a47d5b6
Abhiram <haabhiram@gmail.com>
2022-06-07 21:43:59 +0530

    Weather App

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/api.dart';
+import 'package:description.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor:Colors.black87,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_response != null)
+                  Column(
+                    children: [
+                      Image.network(_response.iconUrl),
+                      Text(
+                        '${_response.tempInfo.temperature}°',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      Text(_response.weatherInfo.description)
+                    ],
+                  ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: SizedBox(
+                    width: 150,
+                    color:Colors.red[400]
+                    child: TextField(
+                        controller: _cityTextController,
+                        decoration: InputDecoration(labelText: 'City'),
+                        textAlign: TextAlign.center),
+                  ),
+                ),
+                ElevatedButton(onPressed: _search, child: Text('Search'))
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void _search() async {
+    final response = await _dataService.getWeather(_cityTextController.text);
+    setState(() => _response = response);
+  }
+}
